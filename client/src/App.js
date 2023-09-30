87c4ac7b5302e5e9d82803a1acb2168c4fc0dfe7
Dilshad Mohammed <dilshadmohammed13@gmail.com>
2023-10-01 00:03:50 +0530

    project created

---
+++ client/src/App.js
@@ -0,0 +1,35 @@
+import logo from './logo.svg';
+import api from './api';
+import './App.css';
+import { useState } from 'react';
+
+
+function App() {
+  const [state,setState] = useState('');
+  api.get('/').then((response)=>{
+    setState(response.data.hello);
+  }).catch((error)=>{
+    console.log(error);
+  })
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Link to react
+        </a>
+        <h2>{state}</h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
