b8b21c5536d409f55d1d3c6c85a039b32568a1b6
Dilshad Mohammed <dilshadmohammed13@gmail.com>
2023-10-07 17:13:20 +0530

    Navbar created

--- client/src/App.js
+++ client/src/App.js
@@ -4,32 +4,11 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 
-
 function App() {
-  const [state,setState] = useState('');
-  api.get('/').then((response)=>{
-    setState(response.data.hello);
-  }).catch((error)=>{
-    console.log(error);
-  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Link to react
-        </a>
-        <Navbar/>
-        <h2>{state}</h2>
-      </header>
+      <Navbar/>
     </div>
   );
 }
