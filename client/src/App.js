9fc2350c18134d7ed80620e603ac872462a2473b
Noel <inbox.noel1107@gmail.com>
2023-10-08 01:48:49 +0530

    About section and Carousel :| added

--- client/src/App.js
+++ client/src/App.js
@@ -4,14 +4,28 @@ import './App.css';
 import { useState } from 'react';
 import Navbar from './Components/Navbar/Navbar';
 import Helplines from './Components/Helplines/Helplines';
+import About from './Components/About/About'
+import ImageSlider from './Components/Carousel/ImageSlider';
+import ImageSliderAuto from './Components/Carousel/ImageSliderAuto';
+import { ImageData } from './Components/Carousel/json/jsonData';
+
 
 function App() {
 
   return (
     <div className="App">
       <Navbar/>
+      <section id="hero">
+      {/* <ImageSlider ImageData={ImageData} /><br /> */}
+      <ImageSliderAuto ImageData={ImageData} SlideInterValTime={2000} />
 
+      </section>
+      <section id="about">
+        <About />
+      </section>
+      <section id="helpline">
         <Helplines/>
+      </section>
     </div>
   );
 }
---
+++ client/src/Components/Carousel/json/jsonData.js
@@ -0,0 +1,26 @@
+import Img1 from '../Slidelist/list1.jpg'
+import Img2 from '../Slidelist/list2.jpg'
+import Img3 from '../Slidelist/list 3.jpg'
+import Img4 from '../Slidelist/list 4.jpg'
+export const ImageData = [
+    {
+        ImageNo: '1/4',
+        ImageName: 'mountains',
+        ImageSrc: Img1
+    },
+    {
+        ImageNo: '2/4',
+        ImageName: 'img_nature',
+        ImageSrc: Img2
+    },
+    {
+        ImageNo: '3/4',
+        ImageName: 'img_snow',
+        ImageSrc: Img3
+    },
+    {
+        ImageNo: '4/4',
+        ImageName: 'img_band_ny',
+        ImageSrc: Img4
+    }
+];
