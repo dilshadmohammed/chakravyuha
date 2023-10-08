f22a599efe260ecec94712914d09c1f1b5f4b10e
Shadow-Reptile <shadorep1105@gmail.com>
2023-10-08 08:06:05 +0530

    Carousel advanced implemented.

--- client/src/Components/Carousel/ImageSliderAuto.js
+++ client/src/Components/Carousel/ImageSliderAuto.js
@@ -2,78 +2,60 @@
 import './ImageSlider.css';
 
 const ImageSliderAuto = (props) => {
-    const SliderProperty = {
-        ImageNo: '',
-        ImageName: '',
-        ImageSrc: ''
+  const SliderProperty = {
+    ImageNo: '',
+    ImageName: '',
+    ImageSrc: ''
+  };
+
+  const [sliderProperty, setSliderProperty] = useState(SliderProperty);
+  const { ImageNo, ImageName, ImageSrc } = sliderProperty;
+  const [countAuto, setCountAuto] = useState(0);
+  const [animationCls, setAnimationCls] = useState('displayBlock');
+
+  const NextClick = () => {
+    setAnimationCls('displayNone');
+    setTimeout(() => {
+      setAnimationCls('displayBlock');
+    }, 100);
+
+    if (countAuto < props.ImageData.length - 1) {
+      setCountAuto(countAuto + 1);
+    } else {
+      setCountAuto(0);
     }
+  };
 
-    const [sliderProperty, setSliderProperty] = useState(SliderProperty);
+  useEffect(() => {
+    setSliderProperty({
+      ImageNo: props.ImageData[countAuto].ImageNo,
+      ImageName: props.ImageData[countAuto].ImageName,
+      ImageSrc: props.ImageData[countAuto].ImageSrc
+    });
+  }, [countAuto]);
 
-    const { ImageNo, ImageName, ImageSrc } = sliderProperty;
+  useEffect(() => {
+    const interval = setInterval(() => {
+      NextClick();
+    }, props.SlideInterValTime + 5000);
 
-    const [countAuto, setCountAuto] = useState(0);
+    return () => clearInterval(interval);
+  }, [countAuto]);
 
-
-    const [animationCls, setAnimationCls] = useState('displayBlock fade');
-
-
-
-    const NextClick = () => {
-
-        setAnimationCls(() => ('displayNone fade'));
-        const myTimeout = setTimeout(() => {
-            setAnimationCls('displayBlock fade')
-        }, 100);
-
-        if (countAuto <= props.ImageData.length - 1) {
-            setCountAuto(countAuto + 1);
-        }
-
-        if (countAuto === props.ImageData.length - 1) {
-            setCountAuto(0);
-        }
-
-    };
-
-    useEffect(() => {
-
-        setSliderProperty((previous) => ({ ...previous, ImageNo: props.ImageData[countAuto].ImageNo, ImageName: props.ImageData[countAuto].ImageName, ImageSrc: props.ImageData[countAuto].ImageSrc }));
-
-    }, [countAuto]);
-
-
-
-    useEffect(() => {
-
-        const interval = setInterval(() => {
-
-            NextClick();
-
-        }, props.SlideInterValTime + 5000);
-
-        return () => clearInterval(interval);
-
-    }, [countAuto]);
-
-
-
-    return (
-        <>
-            <div className='slideshow-container '>
-
-
-                <div className={animationCls}>
-                    <div className="numbertext">{ImageNo}</div>
-                    <img src={ImageSrc} className='imageStyle' alt="Img" />
-                </div>
-                <div className="text">{ImageName}</div>
-            </div>
-
-
-        </>
-
-    );
+  return (
+    <>
+      <div className="slideshow-container">
+        <div className={`${animationCls} fade`}>
+          <div className="numbertext">{ImageNo}</div>
+          <img src={ImageSrc} className="imageStyle" alt="Img" />
+        </div>
+        <div className="text">{ImageName}</div>
+        <div className="overlay">
+          <p>{props.TextData[countAuto]}</p>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default ImageSliderAuto;
